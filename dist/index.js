5135a62079717e9aa0eb7b3ed1b86ef8a4a2e66d
DiegoVeraSarricolea <d.vera.sarricolea@gmail.com>
2021-03-17 11:26:10 -0300

    IJ/DV initial ResourcesAsTable

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,15 @@ Object.defineProperty(exports, "Title", {
     return _Title.default;
   }
 });
+Object.defineProperty(exports, "ResourcesAsTable", {
+  enumerable: true,
+  get: function get() {
+    return _ResourcesAsTable.default;
+  }
+});
 
 var _Title = _interopRequireDefault(require("./Title/Title"));
+
+var _ResourcesAsTable = _interopRequireDefault(require("./ResourcesAsTable/ResourcesAsTable"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
